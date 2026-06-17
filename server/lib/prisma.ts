cfd277398831322fba505b015c9a9b1dc2892f1b
Leonardo Reyes <24hwww@gmail.com>
2026-06-17 20:47:07 -0300

    fix vercel

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
   return new PrismaClient({
-    adapter,
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
     errorFormat: 'pretty',
     log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
   })
