509282b409648405dd3aac4b0ece1f35e87d65e7
Leonardo Reyes <24hwww@gmail.com>
2026-06-18 11:23:31 -0300

    key dinamicas

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
+import { initEncryptionKey } from './lib/crypto'
 
 const queryClient = new QueryClient()
+
+initEncryptionKey().catch(() => {})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
